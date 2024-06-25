c2700b03c33ff07feb3a384d6510d9ed1374be3a
akashmishra09 <akash.mishra1202@gmail.com>
2024-06-25 23:27:04 +0530

    episode 7

---
+++ src/components/Error.js
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+import React from "react";
+
+export const Error = () => {
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <div>
+      <h1>Oops!!!</h1>
+      <h2>Something went wrong</h2>
+      <h3>
+        {err.status} : {err.statusText}
+      </h3>
+    </div>
+  );
+};
